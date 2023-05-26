48b0df4c52b225dc5b872b08366f895e8f56d4ee
Kemal <kemalguedes10@gmail.com>
2023-05-26 10:24:55 -0300

    exemplos de codigo em javascript

---
+++ tipo.js
@@ -0,0 +1,67 @@
+// // tipo number
+
+// const number = 3;
+
+// const num1 = 1;
+// const num2 = 2;
+
+// const opera = num1 - num2;
+
+// console.log(opera)
+
+
+// // pponto flutuanete
+
+// const numeroPontoFlutuante = 3.3;
+// const numeroPontoFlutuantSemZero = .5;
+
+// const novaOperacao = numeroPontoFlutuante / numeroPontoFlutuantSemZero;
+
+// console.log(novaOperacao)
+
+// const alura = "Alura";
+// console.log(alura * novaOperacao)
+
+
+//função em JS sort
+
+// var lista = [10,1, 5, 9, 8, 12, 15];
+
+// lista.sort(); // [1, 10, 12, 15, 5, 8, 9]
+
+// function comparaNumeros(a,b) { 
+//     if (a == b) return 0;
+//      if (a < b) return -1;
+//       if (a > b) return 1; 
+//     }
+
+// console.log(lista.sort(comparaNumeros))
+
+
+// //__________________________________________________________________________
+
+//     const text1 = "Olá mundo!";
+//     const text2 = "Kemal disse: ";
+
+//     console.log(text2 + text1)
+    
+
+    // const cifrao = '\u0024'
+    // const aMaiusculo = '\u0041'
+    // const tique = '\u2705'
+    // const hiragana = '\u3041'
+    
+    // console.log(cifrao)
+    // console.log(aMaiusculo)
+    // console.log(tique)
+    // console.log(hiragana)
+
+
+    const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+console.log(inputMinusculo + cidade)
+
