0c9fde776ce92fc5823f73a49677edfc49a8a8e4
Kemal <kemalguedes10@gmail.com>
2023-05-28 23:31:05 -0300

    Usando FOR e WHILE para os sons

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,7 +1,55 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+// Forma 1 do jeito mais basico  usando WHILE
+
+// function tocaSom (idElementoAudio) {
+//     document.querySelector(idElementoAudio).play();
+// }
+
+// const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+// //while
+// let i = 0;
+
+// while (i < 9) {
+
+//     const tecla = listaDeTeclas[i];
+
+//     const instrumento = tecla.classList[1];
+
+//     //template_string
+//     const idAudio = `#som_${instrumento}`;
+
+//     listaDeTeclas[i].onclick = function () {
+//         tocaSom(idAudio);
+//     }
+
+//     i++;
+// }
+
+// _________________________________________Fim Forma 1______________________________________________________
+
+// Forma 2 de resolver com FOR
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//for
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+
+    const tecla = listaDeTeclas[i];
+
+    const instrumento = tecla.classList[1];
+
+    
+    const idAudio = `#som_${instrumento}`; //template_string
+
+    listaDeTeclas[i].onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
